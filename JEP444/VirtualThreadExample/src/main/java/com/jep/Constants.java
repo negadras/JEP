fed65dea24e4cabf592dd8a571d27a7de63d15b4
nega-xavier <57670753+nega-xavier@users.noreply.github.com>
2024-11-17 20:33:34 +0100

    feat(virtual-threads): implement Pokemon API fetcher with platform and virtual threads
    - Create PokemonService for API interactions
    - Add documentation in index.md summarizing the experiment and results
    
    Refs: JEP-444

---
+++ JEP444/VirtualThreadExample/src/main/java/com/jep/Constants.java
@@ -0,0 +1,6 @@
+package com.jep;
+
+public class Constants {
+
+    public static final int NUM_REQUESTS = 1025;
+}
---
+++ JEP444/VirtualThreadExample/src/main/java/com/jep/VirtualThreadPokemonFetcher.java
@@ -0,0 +1,40 @@
+package com.jep;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.logging.Logger;
+
+import static com.jep.Constants.NUM_REQUESTS;
+import static com.jep.PokemonService.fetchPokemon;
+
+public class VirtualThreadPokemonFetcher {
+
+    private static final Logger LOGGER = Logger.getLogger(VirtualThreadPokemonFetcher.class.getName());
+
+    public static void main( String[] args ) {
+        long startTime = System.currentTimeMillis();
+
+        try (ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor()) {
+            List<Future<?>> futures = new ArrayList<>();
+
+            for (int i = 1; i <= NUM_REQUESTS; i++) {
+                final int pokemonId = i;
+                futures.add(executorService.submit(() -> fetchPokemon(pokemonId)));
+            }
+
+            for (Future<?> future: futures) {
+                try {
+                    future.get();
+                } catch (Exception e) {
+                    LOGGER.severe(e.getMessage());
+                }
+            }
+        }
+
+        long endTime = System.currentTimeMillis();
+        LOGGER.info(STR."Total execution time: \{endTime - startTime} milliseconds");
+    }
+}
